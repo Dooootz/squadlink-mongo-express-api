2f5103e1e006d5c7859ae9e5a3df67182b137e5f
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-04-01 22:16:59 +1000

    middleware

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,12 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+// middleware to handle req, res data
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.use('/api/users', require('./routes/userRoutes'))
 
-app.listen(port, () => console.log(`ITS ALIVE on port ${port}`))
+app.listen(port, () => console.log(`ITS ALIVE on port http://localhost:${port}`))
 
 
