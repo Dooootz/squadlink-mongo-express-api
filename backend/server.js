6d0c24fc0ee2551ea1590d39fd095fd378ab91e8
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-04-01 22:53:12 +1000

    override default error handler

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
@@ -9,6 +10,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/users', require('./routes/userRoutes'))
+
+// override express default error handler
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`ITS ALIVE on port http://localhost:${port}`))
 
