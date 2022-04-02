a2cd680036cf7cfe5c9c643784afeaa3c02f9d45
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-04-02 21:18:25 +1000

    new document fields

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,14 +1,49 @@
 const mongoose = require('mongoose')
 const userSchema = mongoose.Schema(
     {
-        text: {
+        // USER PERSONAL INFO
+        firstname: {
             type: String,
-            required: [true, 'Please add a text value']
+            required: [true, 'Please enter your firstname']
+        },
+        lastname: {
+            type: String,
+            required: [true, 'Please enter your lastname']
+        },
+        email: {
+            type: String,
+            required: [true, 'Please enter your email'],
+            unique: true
+        },
+        password: {
+            type: String,
+            required: [true, 'Please enter a password']
+        },
+        birthdate: {
+            // change datatype prior to functionality
+            type: String, 
+            required: [true, 'Please enter your birthdate']
+        },
+        //PROFILE INFO
+        gamertag: {
+            type: String,
+            required: true 
+        },
+        profileimg: {
+            type: String,
+        },
+        status: {
+            type: Boolean,
+            required: true
+        },
+        aboutme: {
+            type: String,
         },
     }, 
     {
     timestamps: true,
     }
 )
+
 
 module.exports = mongoose.model('Users', userSchema)
