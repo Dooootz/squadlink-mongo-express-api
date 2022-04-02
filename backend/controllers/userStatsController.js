c0b8ba362f25f9550b2a4925d82014e3a4a02e96
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-04-02 13:16:38 +1000

    Stats controller template

---
+++ backend/controllers/userStatsController.js
@@ -0,0 +1,52 @@
+const asyncHandler = require('express-async-handler')
+
+const UserStats = require('../models/userStatsModel')
+
+// @desc    Get user stats by id
+// @route   GET /api/userStats
+// @access  Private
+const getUserStatsById = asyncHandler(async(req,res) => {
+    const getAllUserStats = await UserStats.find(req.params.id)
+    res.status(200).json(getAllUserStats)
+})
+
+// @desc    Create users
+// @route   POST /api/userStats
+// @access  Private
+const createUserStatsById = asyncHandler(async(req,res) => {
+    // req.body.<db column name>
+    if(!req.body.name) {
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
+    const userData = await UserStats.create({
+        text: req.body.name
+    })
+    res.status(201).json(userData)
+})
+
+// @desc    Update user
+// @route   PUT /api/userStats/:id
+// @access  Private
+const updateUserStatsById = asyncHandler(async(req,res) => {
+    const user = await UserStats.findById(req.params.id)
+
+    if(!user){
+        res.status(400)
+        throw new Error('User not found')
+    }
+
+    const updatedUserStats = await UserStats.findByIdAndUpdate(req.params.id, req.body, 
+        {
+            new: true,
+        })
+    res.status(200).json(updatedUserStats)
+})
+
+
+module.exports = {
+    getUserStatsById,
+    createUserStatsById,
+    updateUserStatsById,
+}
