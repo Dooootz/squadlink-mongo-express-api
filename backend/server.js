03fac5d82f2a8db30283623558bddcb5a1271f5e
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-04-02 13:16:24 +1000

    add userStats routes

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/userStats', require('./routes/userStatsRoutes'))
 
 // override express default error handler
 app.use(errorHandler)
