20c41d50ce50b6f6f05fca52628aa8d83ac02644
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-04-03 13:57:36 +1000

    process

---
+++ backend/controllers/logController.js
@@ -0,0 +1,10 @@
+// log functions that will run inside the request, response cycle
+// capture username, user IP, current date & time, total requests counter, request method & request path
+
+// checkTotalRequests per session && per 24h 
+// we want to check the total requests made & measure the request count over a 24h period
+    // if the user makes over 1000 requests within a single session || 24 h period
+    // then calculate the time remaining (24h cycle)
+    // restrict user access rights & permissions for the remaining time 
+
+
