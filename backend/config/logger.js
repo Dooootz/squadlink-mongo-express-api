0bb1562352170b66426637bb5a093207c01816c1
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-04-03 21:47:30 +1000

    cleaned up logger functions

--- backend/config/logger.js
+++ backend/config/logger.js
@@ -4,23 +4,100 @@ const {
     format 
 } = require('winston');
 
+const winston = require('winston')
+
 require('winston-mongodb');
 
-const logger = createLogger({
+
+const userLogs = createLogger({
     transports: [
-        new transports.File({
+        new winston.transports.File({
             filename: 'info.log',
             level: 'info',
-            format: format.combine(format.timestamp(), format.json())
+            format: format.combine(format.timestamp(), format.json()),
         }),
-        new transports.MongoDB({
-            level:'error',
+        new winston.transports.MongoDB({
+            level:'info',
             db: process.env.MONGO_URI,
             collection: 'logs',
-            format: format.combine(format.timestamp(), format.json())
+            format: format.combine(format.timestamp(), format.json()),
         })
     ]
 
 })
 
-module.exports = logger
+module.exports = userLogs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// deprecated ?
+// ---  --- --- --- --- --- --- ---
+
+// const winston = require('winston')
+
+// const logFormatter = (params) => {
+//     return `${params.timestamp} [${params.level}] ${params.message}` 
+// }
+
+// const createLogger = () => {
+//     winston.addColors({
+//         error: 'red',
+//         warn: 'darkred',
+//         info: 'cyan',
+//         debug: 'gray'
+//     })
+
+//     return winston.createLogger({
+//         level: 'info',
+//         format: winston.format.json(),
+//         defaultMeta: { service: "http-api"},
+//         transports: [
+//             new winston.transports.Console({
+//                 format: winston.format.combine(
+//                     winston.format.colorize(),
+//                     winston.format.timestamp(),
+//                     winston.format.splat(),
+//                     winston.format.printf(logFormatter)
+//                 )
+//             })
+//         ]
+//     })
+// }
+
+
+// module.exports = {
+//     logFormatter,
+//     logger: createLogger()
+// }
+
+// ---  --- --- --- --- --- --- ---
+// var winston = require('winston');
+
+// /**
+//  * Requiring `winston-mongodb` will expose
+//  * `winston.transports.MongoDB`
+//  */
+// require('winston-mongodb');
+
+// winston.add(new winston.transports.MongoDB(
+
+// ));
+// ---  --- --- --- --- --- --- ---
+
